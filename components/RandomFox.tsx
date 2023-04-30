18c1d99b3d5085ee6226f8b9de10135a45e42103
Ezequiel Emanuel Ramirez <ezequielram@gmail.com>
2023-04-30 10:53:05 -0300

    types en useState

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,11 +1,9 @@
-//generate a random function between 1 and 123
-
-const random = (min:number, max:number):number => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+type Props = {
+    image: string
 }
 
-export const RandomFox = ():JSX.Element => {
-    const image = `https://randomfox.ca/images/${random(1, 123)}.jpg`;
+export const RandomFox = ({image}:Props):JSX.Element => {
+    
     
     return <img width={320} height='auto' src={image} alt="random fox" className="rounded-lg shadow-lg"/>
 }
